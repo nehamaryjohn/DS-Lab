093ceee471950ab0605039c600be0915b70fe3b1
nehamaryjohn <nehamary996@gmail.com>
2025-07-18 12:28:21 +0530

    Add files via upload

---
+++ array1.py
@@ -0,0 +1,7 @@
+import numpy as np
+ar_name=np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
+print(ar_name[1:4])
+print(ar_name[0:-1])
+print(ar_name[1:3,0:2])
+print(ar_name[ : ,1:3])
+print(ar_name[0: ,1:3])
---
+++ array2.py
@@ -0,0 +1,14 @@
+import numpy as np
+arr1=([[1,2],[3,4]])
+arr2=([[5,6],[9,10]])
+print(arr1)
+print(arr2)
+print(np.add(arr1,arr2))
+print(np.subtract(arr1,arr2))
+print(np.multiply(arr1,arr2))
+print(np.divide(arr1,arr2))
+print(np.dot(arr1,arr2))
+print(arr1.transpose())
+print(arr1.trace())
+
+
